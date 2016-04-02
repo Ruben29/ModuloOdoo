fd413a167f037c2d8bbb4b1560f15ce0e1f0079f
Rubén 29 <Rubn 29@DESKTOP-0SH9SO1>
2016-04-02 17:40:46 +0200

    Iniciando Modulo Odoo

---
+++ openacademy/models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from openerp import api
+from openerp import fields
+from openerp import models
+
+# class openacademy(models.Model):
+#     _name = 'openacademy.openacademy'
+
+#     name = fields.Char()
