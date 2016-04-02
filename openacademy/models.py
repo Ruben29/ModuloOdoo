0a1a6c35da77f7450bd0777bd7419083a53a03e9
Rubén 29 <Rubn 29@DESKTOP-0SH9SO1>
2016-04-02 17:51:08 +0200

    Creados "Courses" y "Sessions"

--- openacademy/models.py
+++ openacademy/models.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
-
+ 
 from openerp import api
 from openerp import fields
 from openerp import models
-
-# class openacademy(models.Model):
-#     _name = 'openacademy.openacademy'
-
-#     name = fields.Char()
+ 
+class Course(models.Model):
+    _name = 'openacademy.course'
+ 
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+ 
+class Session(models.Model):
+    _name = 'openacademy.session'
+ 
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
