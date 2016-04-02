e2d7fe86e8e546f526e8791281a3f3fd7b510ab9
Rubén 29 <Rubn 29@DESKTOP-0SH9SO1>
2016-04-02 18:18:09 +0200

    9º commit: Cambiado el TreeView de las Sessions, ahora indican por colores la duración de las clases

--- openacademy/models.py
+++ openacademy/models.py
@@ -3,6 +3,7 @@
 from openerp import api
 from openerp import fields
 from openerp import models
+from openerp import esceptions
  
 class Course(models.Model):
     _name = 'openacademy.course'
@@ -12,10 +13,36 @@ class Course(models.Model):
     
     
     responsible_id = fields.Many2one('res.users',
-        ondelete='set null', string="Responsible", index=True)
+                                     ondelete='set null', string="Responsible", index=True)
          
     session_ids = fields.One2many(
-        'openacademy.session', 'course_id', string="Sessions")
+                                  'openacademy.session', 'course_id', string="Sessions")
+        
+    @api.multi
+    def copy(self, default=None):
+        default = dict(default or {})
+ 
+        copied_count = self.search_count(
+                                         [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+ 
+        default['name'] = new_name
+        return super(Course, self).copy(default)
+ 
+         
+     
+    _sql_constraints = [
+        ('name_description_check',
+         'CHECK(name != description)',
+         "The title of the course should not be the description"),
+ 
+        ('name_unique',
+         'UNIQUE(name)',
+         "The course title must be unique"),
+    ]
  
  
 class Session(models.Model):
@@ -29,11 +56,11 @@ class Session(models.Model):
 
     
     instructor_id = fields.Many2one('res.partner', string="Instructor",
-        domain=['|', ('instructor', '=', True),
-                     ('category_id.name', 'ilike', "Teacher")])
+                                    domain=['|', ('instructor', '=', True),
+                                    ('category_id.name', 'ilike', "Teacher")])
     
     course_id = fields.Many2one('openacademy.course',
-        ondelete='cascade', string="Course", required=True)
+                                ondelete='cascade', string="Course", required=True)
         
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     
